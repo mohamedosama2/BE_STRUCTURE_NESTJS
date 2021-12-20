ee0bb2d79e35a456c19732d7f3e02812f318d930
Remah Amr <remah.alarabi@hamza.solutions>
2021-12-20 17:56:59 +0200

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       script: './dist/main.js',
       instances: '1',
       autorestart: false,
-      watch: true,
+      watch: false,
       max_memory_restart: '1G',
       exec_mode: 'cluster',
     },
