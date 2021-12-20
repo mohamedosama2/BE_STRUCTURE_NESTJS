01ec2960466cff48401bff4706540df408ae8db3
Remah Amr <remah.alarabi@hamza.solutions>
2021-12-20 17:46:51 +0200

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,6 @@
 module.exports = {
   apps: [
     {
-      name: 'Nest App',
-      script: './dist/main.js',
-      instances: '1',
-      autorestart: false,
-      watch: true,
-      max_memory_restart: '1G',
-      exec_mode: 'cluster',
-      env: {
-        NODE_ENV: 'development',
-      },
-      env_production: {
-        NODE_ENV: 'production',
-      },
-    },
-    {
       name: 'Consumer',
       script: './dist/message-queue/consumer.service.js',
       instances: 1,
