042e6c00dc1ef7054d0611a3f5acf8264d540986
Remah Amr <remah.alarabi@hamza.solutions>
2021-12-30 05:04:01 +0200

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,11 +6,61 @@ module.exports = {
       instances: '2',
       exec_mode: 'cluster',
       autorestart: true,
+      env_production: {
+        NODE_ENV: 'production',
+        MONGODB_URI:
+          'mongodb+srv://Admin:Admin123456@bassthalk.7nkfp.mongodb.net/dbtest?retryWrites=true&w=majority',
+        api_key: 'remah',
+        CLOUDINARY_URL:
+          'cloudinary://716494354337253:x5gdQ2S2bshPvJ0YmvEwd4715YQ@hamza2021',
+        JWT_SECRET: 'supersecret',
+        clientID:
+          '1017089558534-sg5lphbqs179g9fm3b5qcrigl6l2br20.apps.googleusercontent.com',
+        clientSecret: 'd2q1gNN96PNi5Xyb-gI3PFGN',
+        TWILIO_AUTH_TOKEN: '21260d2db9e10751a20c5c10a227244b',
+        TWILIO_ACCOUNT_SID: 'AC2a7cefcd9c7c7714440d00eca7aa7720',
+        TWILIO_ACCOUNT_VERIFY_SID: 'VAdf43c41c1e54c08f202f283e0b100e47',
+        facebookUrl:
+          'https://graph.facebook.com/v2.12/me?fields=id,name,email,gender,picture{url}',
+        cloud_name: 'dqfrk92mp',
+        api_key: '253596391863857',
+        api_secret: 'zUSdqm663H7lfMKqdirIGZxu318',
+        AMQP_URL2:
+          'amqps://zcfvyzvh:cKIn4m4ZbwR-Y0lmhAfOFcmlnksvzed5@fox.rmq.cloudamqp.com/zcfvyzvh?heartbeat:60',
+        AMQP_URL:
+          'amqps://kmuergjm:Q3oBdA_iX3triTcv_2YopwdL4AtHkz8D@jaguar.rmq.cloudamqp.com/kmuergjm?heartbeat=45',
+        PORT: 8080,
+      },
     },
     {
       name: 'Consumer',
       script: './dist/message-queue/worker.js',
       instances: 1,
+      env_production: {
+        NODE_ENV: 'production',
+        MONGODB_URI:
+          'mongodb+srv://Admin:Admin123456@bassthalk.7nkfp.mongodb.net/dbtest?retryWrites=true&w=majority',
+        api_key: 'remah',
+        CLOUDINARY_URL:
+          'cloudinary://716494354337253:x5gdQ2S2bshPvJ0YmvEwd4715YQ@hamza2021',
+        JWT_SECRET: 'supersecret',
+        clientID:
+          '1017089558534-sg5lphbqs179g9fm3b5qcrigl6l2br20.apps.googleusercontent.com',
+        clientSecret: 'd2q1gNN96PNi5Xyb-gI3PFGN',
+        TWILIO_AUTH_TOKEN: '21260d2db9e10751a20c5c10a227244b',
+        TWILIO_ACCOUNT_SID: 'AC2a7cefcd9c7c7714440d00eca7aa7720',
+        TWILIO_ACCOUNT_VERIFY_SID: 'VAdf43c41c1e54c08f202f283e0b100e47',
+        facebookUrl:
+          'https://graph.facebook.com/v2.12/me?fields=id,name,email,gender,picture{url}',
+        cloud_name: 'dqfrk92mp',
+        api_key: '253596391863857',
+        api_secret: 'zUSdqm663H7lfMKqdirIGZxu318',
+        AMQP_URL2:
+          'amqps://zcfvyzvh:cKIn4m4ZbwR-Y0lmhAfOFcmlnksvzed5@fox.rmq.cloudamqp.com/zcfvyzvh?heartbeat:60',
+        AMQP_URL:
+          'amqps://kmuergjm:Q3oBdA_iX3triTcv_2YopwdL4AtHkz8D@jaguar.rmq.cloudamqp.com/kmuergjm?heartbeat=45',
+        PORT: 8080,
+      },
     },
   ],
   deploy: {
@@ -21,7 +71,8 @@ module.exports = {
       ref: 'pm2',
       repo: 'https://github.com/Remah-Amr/BE_STRUCTURE_NESTJS.git',
       path: '/var/www/production1',
-      'post-deploy': 'npm install && pm2-runtime start ecosystem.config.js',
+      'post-deploy':
+        'npm install && npm run build && pm2-runtime start ecosystem.config.js --env production',
     },
   },
 };
