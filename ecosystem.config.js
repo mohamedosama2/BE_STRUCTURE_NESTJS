1414444adb587013d088c1f7a8d29ecf516976c0
Remah Amr <remah.alarabi@hamza.solutions>
2021-12-30 05:09:32 +0200

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -72,7 +72,7 @@ module.exports = {
       repo: 'https://github.com/Remah-Amr/BE_STRUCTURE_NESTJS.git',
       path: '/var/www/production1',
       'post-deploy':
-        'npm install && npm run build && pm2-runtime start ecosystem.config.js --env production',
+        'npm cache clean --force && rm -rf node_modules && npm install && npm run build && pm2-runtime start ecosystem.config.js --env production',
     },
   },
 };
