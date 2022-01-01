8d2fe0b6426dfe929a704aaf856b3e065ed38735
Remah Amr <remah.alarabi@hamza.solutions>
2022-01-01 22:51:35 +0200

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -71,10 +71,10 @@ module.exports = {
     production: {
       // "key"  : ".ssh/creds.pem",
       user: 'root',
-      host: ['165.227.107.178'],
+      host: ['142.93.178.238'],
       ref: 'pm2',
       repo: 'https://github.com/Remah-Amr/BE_STRUCTURE_NESTJS.git',
-      path: '/var/www/production2',
+      path: '/var/www/production1',
       'post-deploy':
         'npm install && npm run build &&  pm2 reload ecosystem.config.js --env production && pm2 save',
     },
